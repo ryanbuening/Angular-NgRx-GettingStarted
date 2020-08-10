6ae5a78c8d7444dd5ab9dd8fc2833eecb9cf9d0b
Ryan Buening <ryan.buening@celinainsurance.com>
2020-08-10 08:31:34 -0400

    after Demo: Communicating with the Edit Component

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -1,18 +1,17 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
-
-import { Subscription } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { Store } from '@ngrx/store';
-import { State, getShowProductCode } from '../state/product.reducer';
+import { State, getShowProductCode, getCurrentProduct } from '../state/product.reducer';
+import * as ProductActions from '../state/product.actions';
 
 @Component({
 	selector: 'pm-product-list',
 	templateUrl: './product-list.component.html',
 	styleUrls: ['./product-list.component.css']
 })
-export class ProductListComponent implements OnInit, OnDestroy {
+export class ProductListComponent implements OnInit {
 	pageTitle = 'Products';
 	errorMessage: string;
 
@@ -22,12 +21,16 @@ export class ProductListComponent implements OnInit, OnDestroy {
 
 	// Used to highlight the selected product in the list
 	selectedProduct: Product | null;
-	sub: Subscription;
 
 	constructor(private store: Store<State>, private productService: ProductService) { }
 
 	ngOnInit(): void {
-		this.sub = this.productService.selectedProductChanges$.subscribe(
+		// this.sub = this.productService.selectedProductChanges$.subscribe(
+		// 	currentProduct => this.selectedProduct = currentProduct
+		// );
+
+		// TODO: Unsubscribe
+		this.store.select(getCurrentProduct).subscribe(
 			currentProduct => this.selectedProduct = currentProduct
 		);
 
@@ -42,24 +45,22 @@ export class ProductListComponent implements OnInit, OnDestroy {
 		);
 	}
 
-	ngOnDestroy(): void {
-		this.sub.unsubscribe();
-	}
-
 	checkChanged(): void {
 		// this.displayCode = !this.displayCode;
 
-		this.store.dispatch({
-			type: '[Product] Toggle Product Code'
-		});
+		this.store.dispatch(ProductActions.toggleProductCode());
 	}
 
 	newProduct(): void {
-		this.productService.changeSelectedProduct(this.productService.newProduct());
+		// this.productService.changeSelectedProduct(this.productService.newProduct());
+
+		this.store.dispatch(ProductActions.initializeCurrentProduct());
 	}
 
 	productSelected(product: Product): void {
-		this.productService.changeSelectedProduct(product);
+		// this.productService.changeSelectedProduct(product);
+
+		this.store.dispatch(ProductActions.setCurrentProduct({ product }));
 	}
 
 }
--- APM-Demo0/src/app/products/state/product.reducer.ts
+++ APM-Demo0/src/app/products/state/product.reducer.ts
@@ -1,6 +1,8 @@
 import { createReducer, createAction, on, createFeatureSelector, createSelector } from '@ngrx/store';
 import * as AppState from '../../state/app.state';
 import { Product } from '../product';
+import { toggleProductCode } from './product.actions';
+import * as ProductActions from './product.actions';
 
 export interface State extends AppState.State {
 	products: ProductState;
@@ -32,10 +34,34 @@ export const getProducts = createSelector(getProductFeatureState, (state) => sta
 
 export const productReducer = createReducer<ProductState>(
 	initialState,
-	on(createAction('[Product] Toggle Product Code'), (state): ProductState => {
+	on(ProductActions.toggleProductCode, (state): ProductState => {
 		return {
 			...state,
 			showProductCode: !state.showProductCode
+		};
+	}),
+	on(ProductActions.setCurrentProduct, (state, action): ProductState => {
+		return {
+			...state,
+			currentProduct: action.product
+		}
+	}),
+	on(ProductActions.clearCurrentProduct, (state): ProductState => {
+		return {
+			...state,
+			currentProduct: null
+		};
+	}),
+	on(ProductActions.initializeCurrentProduct, (state): ProductState => {
+		return {
+			...state,
+			currentProduct: {
+				id: 0,
+				productName: '',
+				productCode: 'New',
+				description: '',
+				starRating: 0
+			}
 		};
 	})
 );
